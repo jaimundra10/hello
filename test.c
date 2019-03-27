6b2fbd6c6f8178bf8d7284695a7cf3cbecd9ee63
jaimundra10 <48976723+jaimundra10@users.noreply.github.com>
2019-03-27 12:22:00 +0530

    Update test.c

--- test.c
+++ test.c
@@ -1,32 +1,7 @@
-#include<stdio.h>
-#include"i.h"
+#include <stdio.h>
 int main()
 {
-
-    int p,q,r;
-
-    for(int i=0;i<50;i++)
-    {
-        if(a[i]==a[i+1])
-        {
-           p++;
-
-           printf("sample no:%d || voltage level:%d || level\n ",i,a[i]);
-        }
-        else if(a[i]==0&&a[i+1]==1)
-        {
-           q++;
-           printf("sample no:%d || voltage level:%d || rising\n ",i,a[i]);
-        }
-        else
-        {
-            r++;
-           printf("sample no:%d || voltage level:%d || falling \n",i,a[i]);
-        }
-
-
-    }
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
 }
-
-​
-
